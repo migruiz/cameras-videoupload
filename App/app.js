28fe7084f0f849ef7905ef55fbc8efa03beb913e
miguel <mig.ruiz@gmail.com>
2019-07-11 15:16:17 +0100

    throttling readings

--- App/app.js
+++ App/app.js
@@ -1,6 +1,7 @@
 const { interval,Observable } = require('rxjs');
-const { throttleTime,map } = require('rxjs/operators');
+const { throttleTime,map,groupBy,mergeMap } = require('rxjs/operators');
  
+
 if (process.env.MQTTLOCAL==null)
     process.env.MQTTLOCAL='mqtt://localhost:1884'
 
@@ -9,15 +10,33 @@ if (process.env.MQTTLOCAL==null)
 var mqtt = require('./mqttCluster.js');
 global.mtqqLocalPath = process.env.MQTTLOCAL;
 global.videoSegmentTopic = 'videoSegmentTopic';
+global.sensorReadingTopic = 'sensorReadingTopic';
 
-const videoFileStream = new Observable(async subscriber => {      
+
+const videoFileStream = new Observable(async subscriber => {  
     var mqttCluster=await mqtt.getClusterAsync()   
     mqttCluster.subscribeData(global.videoSegmentTopic, function(content){
         subscriber.next(content)
     });
 });
-const filteredStream = videoFileStream.pipe(map(val => val.format.filename))
+const sensorsReadingStream = new Observable(async subscriber => {  
+    var mqttCluster=await mqtt.getClusterAsync()   
+    mqttCluster.subscribeData(global.sensorReadingTopic, function(content){
+        subscriber.next(content)
+    });
+});
+
+
+const filteredStream = videoFileStream.pipe(map(val => ({fileLocation :val.format.filename})))
+
+const throttledReadingsStreams = sensorsReadingStream.pipe(
+    groupBy(p => p.data, p => p),    
+    mergeMap(s => s.pipe(throttleTime(4000))),        
+)
+
+
 filteredStream.subscribe(val => console.log(JSON.stringify(val)));   
+throttledReadingsStreams.subscribe(reading => console.log(JSON.stringify(reading)));   
 console.log("running");
 
 
