00ab0adc420dbd18c592fa817633d3e8b6e9960f
miguel <mig.ruiz@gmail.com>
2019-07-11 16:35:11 +0100

    working with video strem

--- App/app.js
+++ App/app.js
@@ -1,6 +1,9 @@
 const { interval,Observable } = require('rxjs');
-const { throttleTime,map,groupBy,mergeMap } = require('rxjs/operators');
- 
+const { throttleTime,map,groupBy,mergeMap,scan,filter } = require('rxjs/operators');
+const path = require('path');
+
+
+
 
 if (process.env.MQTTLOCAL==null)
     process.env.MQTTLOCAL='mqtt://localhost:1884'
@@ -27,15 +30,41 @@ const sensorsReadingStream = new Observable(async subscriber => {
 });
 
 
-const filteredStream = videoFileStream.pipe(map(val => ({fileLocation :val.format.filename})))
+const videoStream = videoFileStream.pipe(
+    map(v => (
+        {
+            fileName:v.format.filename,
+            startTime:parseInt(path.basename(v.format.filename,'.mp4')),            
+            length:Math.round(parseFloat(v.format.duration)), 
+        }
+    )),
+    map(v => Object.assign({endTime:v.startTime+v.length}, v)),
+    scan((acc, curr) => (
+        {
+            previous:acc.current,
+            current:curr,  
+        }
+    ), {}),
+    filter( v => v.previous!=null),
+    map ( v => ({
+        startTime:v.previous.startTime,
+        endTime:v.current.endTime,
+        filesToJoin: [v.previous.fileName, v.current.fileName]
+    }))
+)
+
+
 
 const throttledReadingsStreams = sensorsReadingStream.pipe(
-    groupBy(p => p.data, p => p),    
+    groupBy(r => r.data, r => r),    
     mergeMap(s => s.pipe(throttleTime(4000))),        
 )
 
 
-filteredStream.subscribe(val => console.log(JSON.stringify(val)));   
+videoStream.subscribe(val => {
+    console.log(JSON.stringify(val));
+    console.log(Math.round((new Date).getTime()/1000));
+});   
 throttledReadingsStreams.subscribe(reading => console.log(JSON.stringify(reading)));   
 console.log("running");
 
